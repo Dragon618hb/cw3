a8fe732fa87ebed5964f247794f5a0baebba8360
Joyce <joyce@JoycedeMacBook-Pro.local>
2026-05-09 23:34:01 +0100

    Add 10 AD classification papers

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,117 @@
 ﻿define({ entries : {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "corder1993apoe": {
+        "author": "Corder, Elizabeth H. and Saunders, Ann M. and Strittmatter, Warren J.",
+        "doi": "10.1126/science.8346443",
+        "journal": "Science",
+        "keywords": "type:genetics, topic:apoe, topic:risk-factor",
+        "number": "5123",
+        "pages": "921--923",
+        "title": "Gene dose of apolipoprotein E type 4 allele and the risk of Alzheimer's disease in late onset families",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "261",
+        "year": "1993"
+    },
+    "dimitriadis2018random": {
+        "author": "Dimitriadis, Stavros I. and Liparas, Dimitris",
+        "doi": "10.4103/1673-5374.233433",
+        "journal": "Neural Regeneration Research",
+        "keywords": "type:mri-ml, topic:random-forest, topic:mri",
+        "number": "6",
+        "pages": "962--962",
+        "title": "How random is the random forest? Random forest algorithm on the service of structural imaging biomarkers for Alzheimer's disease",
+        "type": "article",
+        "volume": "13",
+        "year": "2018"
+    },
+    "dong2019ensemble": {
+        "author": "Dong, Xibin and Yu, Zhiwen and Cao, Wenming and Shi, Yifan and Ma, Qianli",
+        "doi": "10.1007/s11704-019-8208-z",
+        "journal": "Frontiers of Computer Science",
+        "keywords": "type:deep-learning, topic:ensemble, topic:stacking, topic:meta-learning",
+        "number": "2",
+        "pages": "241--258",
+        "title": "A survey on ensemble learning",
+        "type": "article",
+        "volume": "14",
+        "year": "2019"
+    },
+    "ju2017deep": {
+        "author": "Ju, Ronghui and Hu, Chenhui and Zhou, Peng and Li, Quanzheng",
+        "doi": "10.1109/tcbb.2017.2776910",
+        "journal": "IEEE/ACM Transactions on Computational Biology and Bioinformatics",
+        "keywords": "type:deep-learning, topic:deep-learning, topic:mri",
+        "number": "1",
+        "pages": "244--257",
+        "title": "Early Diagnosis of Alzheimer's Disease Based on Resting-State Brain Networks and Deep Learning",
+        "type": "article",
+        "volume": "16",
+        "year": "2017"
+    },
+    "kim2010gwas": {
+        "author": "Kim, Sungeun and Shen, Li and Swaminathan, Shanker",
+        "doi": "10.1212/wnl.0b013e318204a397",
+        "journal": "Neurology",
+        "keywords": "type:genetics, topic:gwas, topic:snp",
+        "number": "1",
+        "pages": "69--79",
+        "title": "Genome-wide association study of CSF biomarkers A\\ss 1-42, t-tau, and ptau181p in the ADNI cohort",
+        "type": "article",
+        "volume": "76",
+        "year": "2010"
+    },
+    "kruthika2019multistage": {
+        "author": "Kruthika, K. R. and Rajeswari and Maheshappa, H. D.",
+        "doi": "10.1016/j.imu.2018.12.003",
+        "journal": "Informatics in Medicine Unlocked",
+        "keywords": "type:mri-ml, topic:classification, topic:cn-mci-boundary",
+        "pages": "34--42",
+        "title": "Multistage classifier-based approach for Alzheimer's disease prediction and retrieval",
+        "type": "article",
+        "volume": "14",
+        "year": "2019"
+    },
+    "ktnp2018svm": {
+        "author": "K.T.N.P, Kavitha and Varghese, Divya",
+        "booktitle": "2018 2nd International Conference on Trends in Electronics and Informatics (ICOEI)",
+        "doi": "10.1109/icoei.2018.8553789",
+        "keywords": "type:mri-ml, topic:svm, topic:mri",
+        "pages": "895--898",
+        "title": "A Novel Approach for Diagnosing Alzheimer's Disease Using SVM",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "shen2010imaging": {
+        "author": "Shen, Li and Kim, Sungeun and Risacher, Shannon L.",
+        "doi": "10.1016/j.neuroimage.2010.01.042",
+        "journal": "NeuroImage",
+        "keywords": "type:genetics, topic:gwas, topic:mri, topic:multimodal",
+        "number": "3",
+        "pages": "1051--1063",
+        "title": "Whole genome association study of brain-wide imaging phenotypes for identifying quantitative trait loci in MCI and AD",
+        "type": "article",
+        "volume": "53",
+        "year": "2010"
+    },
+    "weiner2011adni": {
+        "author": "Weiner, Michael W. and Aisen, Paul S. and Jack, Clifford R.",
+        "doi": "10.1016/j.jalz.2011.09.172",
+        "journal": "Alzheimer's \\& Dementia",
+        "keywords": "type:dataset, topic:adni, topic:multimodal",
+        "number": "1S",
+        "title": "The Alzheimer's Disease Neuroimaging Initiative: A review of papers published since its inception",
+        "type": "article",
+        "volume": "8",
+        "year": "2011"
+    },
+    "ye2012sparse": {
+        "author": "Ye, Jieping and Farnum, Michael and Yang, Eric",
+        "doi": "10.1186/1471-2377-12-46",
+        "journal": "BMC Neurology",
+        "keywords": "type:genetics, topic:mci-conversion, topic:feature-selection",
+        "number": "1",
+        "title": "Sparse learning and stability selection for predicting MCI to AD conversion using baseline ADNI data",
+        "type": "article",
+        "volume": "12",
+        "year": "2012"
     }
 }});
